7d9baf0c9eac991b27ccc8b009af10ca45682bab
Upasara <mihirangaupasara2000@gmail.com>
2025-02-14 13:14:33 +0530

    changes

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -6,24 +6,6 @@ const userContext = createContext();
 const authContext = ({ children }) => {
  const [user, setUser] = useState(null);
 
- const navigate = useNavigate();
-
- useEffect(() => {
-  const verifyUser = async () => {
-   try {
-    const response = await axios.get('http://localhost:5000/api/auth.verify');
-    if (response.data.success) {
-     setUser(response.data.user);
-    }
-   } catch (error) {
-    if (error.response && !error.response.data.error) {
-     navigate('/login');
-    }
-   }
-  };
-  verifyUser();
- }, []);
-
  const login = (user) => {
   setUser(user);
  };
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
 const Admin = () => {
- const navigate = useNavigate();
- const handleButton = () => {
-  navigate('/login');
- };
  const { user } = useAuth();
 
- return (
-  <div className='text-center mt-10'>
-   Hello {user && user.name}
-   <div className='flex items-center justify-center mt-20'>
-    <button
-     className='border px-4 py-2 shadow-xl bg-primaryDark text-white rounded-md'
-     onClick={handleButton}
-    >
-     LOGIN
-    </button>
-   </div>
-  </div>
- );
+ return <div>Hello , {user && user.name}</div>;
 };
 
 export default Admin;
