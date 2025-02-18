6bac2ee69b625105e806efe26137b4a4a881f1e3
Upasara <mihirangaupasara2000@gmail.com>
2025-02-18 16:16:03 +0530

    middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwtWebToken from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const veirfyUser = async (req, res, next) =>{
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        if(!token){
+            return res.status(404).json({success:false, error: "Token not Provided"}) 
+        }
+
+        const decoded =  jwtWebToken.verify(token, process.env.JWT_KEY)
+        if(!decoded){
+            return res.status(404).json({success: false, error: "Token not valid"})
+        }
+
+        const user = await User.findById({_id:decoded._id}).select('-password')
+        if(!user){
+            return res.status(404).json({success:false, error: "User not found"})
+        }
+
+        req.user = user
+        next()
+    }catch(error){
+        return res.status(500).json({success:false, error: "Server Error"})
+    }
+}
+
+export default veirfyUser;
--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -5,6 +6,33 @@ const userContext = createContext();
 
 const authContext = ({ children }) => {
  const [user, setUser] = useState(null);
+
+ const navigate = useNavigate();
+
+ useEffect(() => {
+  const verifyUser = async () => {
+   try {
+    const token = localStorage.getItem('token');
+    if (token) {
+     const response = await axios.get('http://localhost:5000/api/auth/verify', {
+      headers: {
+       Authorization: `Bearer ${token}`,
+      },
+     });
+     if (response.data.success) {
+      setUser(response.data.user);
+     }
+    } else {
+     navigate('/login');
+    }
+   } catch (error) {
+    if (error.response && !error.response.data.error) {
+     navigate('/login');
+    }
+   }
+  };
+  verifyUser();
+ }, []);
 
  const login = (user) => {
   setUser(user);
