7a6558844961812d8b26af55576090e9c54947a1
Upasara <mihirangaupasara2000@gmail.com>
2025-04-28 16:39:48 +0530

    overview

--- src/components/dashboard/AdminSummary.jsx
+++ src/components/dashboard/AdminSummary.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import SummaryCard from './SummaryCard';
-import { FaUsers } from 'react-icons/fa';
+import { FaBuilding, FaUsers } from 'react-icons/fa';
 
 const AdminSummary = () => {
  return (
-  <div>
-   <h3>Dashboard</h3>
-   <div>
-    <SummaryCard icon={<FaUsers />} text='Total Employees' number={13} />
+  <div className='p-6'>
+   <h3 className='text-2xl font-bold'>Dashboard</h3>
+   <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-10'>
+    <SummaryCard
+     icon={<FaUsers />}
+     text='Total Employees'
+     number={13}
+     color='bg-primaryDark'
+    />
+    <SummaryCard
+     icon={<FaBuilding />}
+     text='Total Departments'
+     number={5}
+     color='bg-primaryDark'
+    />
    </div>
   </div>
  );
--- src/components/dashboard/SummaryCard.jsx
+++ src/components/dashboard/SummaryCard.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const SummaryCard = ({ icon, text, number }) => {
  return (
-  <div>
-   <div>{icon}</div>
+  <div className='rounded-md flex bg-white shadow-md '>
+   <div className='text-3xl flex justify-center items-center  text-primaryDark px-3'>
+    {icon}
+   </div>
    <div>
     <p>{text}</p>
     <p>{number}</p>
